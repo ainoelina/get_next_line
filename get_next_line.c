229dea8b9ad94cb7109a29a6966e907d288bfa8f
Aino Vuorio <avuorio@f0r4s21.codam.nl>
2020-11-24 17:25:01 +0100

    memorystuff

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: avuorio <avuorio@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/17 12:37:07 by avuorio       #+#    #+#                 */
-/*   Updated: 2020/11/24 13:56:10 by avuorio       ########   odam.nl         */
+/*   Updated: 2020/11/24 17:24:18 by avuorio       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,8 @@ static char		*str_new(size_t size)
 
 static void		ft_strdel(char **as)
 {
-	if (as)
-	{
-		free(*as);
-		*as = NULL;
-	}
+	free(*as);
+	*as = NULL;
 }
 
 static int		string_output(int fd, char **line, char **array)
@@ -118,4 +115,41 @@ int				get_next_line(int fd, char **line)
 	if (reader == 0 && array[fd] == NULL)
 		return (0);
 	return (string_output(fd, line, array));
+}
+
+int main(int argc, char **argv)
+{
+    int fd;
+    int ret;
+    int line;
+    char *buff;
+    line = 0;
+    if (argc == 2)
+    {
+        //open returns fd or -1 if it failed to open the file
+        fd = open(argv[1], O_RDONLY);
+        while ((ret = get_next_line(fd, &buff)) > 0)
+        {
+            printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+            free(buff);
+        }
+        printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+        if (ret == -1)
+            printf("-----------\nError\n");
+        else if (ret == 0)
+            printf("-----------\nEnd of file\n");
+//		free(buff);
+        close(fd);
+    }
+    if (argc == 1)
+    {
+        while ((ret = get_next_line(0, &buff)) > 0)
+            printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+        if (ret == -1)
+            printf("-----------\nError\n");
+        else if (ret == 0)
+            printf("-----------\nEnd of stdin\n");
+        close(fd);
+    }
+    return (0);
 }
